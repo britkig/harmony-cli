2ff8134a0d6c8f484acd21183983312d93e620c4
BritKig <97478354+britkig@users.noreply.github.com>
2026-06-11 20:10:17 +0100

    Update .harmony.php

--- .cli/.harmony.php
+++ .cli/.harmony.php
@@ -1,4 +1,18 @@
-<?php namespace Console;
-require_once '.harmony.php';
-function Log(string $m, ?string $c){
-	echo \String\Format('%s	%s	%s',\date(\DATE_ISO8601),$c,$m).\PHP_EOL;}
+<?php namespace CLI;
+require_once \dirname( __DIR__).'/.harmony.php';
+const Root=__DIR__;
+
+function Log(string $message, ?string $group=null):void{
+	echo \String\Format('%s	%s	%s',\date(\DATE_ISO8601), $group, $message).\PHP_EOL;
+}
+
+\spl_autoload_register(callback:function(string$_):void{
+	if(\is_file($_=\implode(\System\DirSeparator,[Root,\strtolower($_).'.php'])))
+		\System\SecureInclude($_);
+	}, prepend:true);
+
+\IO\Search($_, \String\JoinA(\System\DirSeparator, Root, '.function'), '/.*\.php$/', true);
+foreach($_ as $a)
+	\System\SecureInclude($a);
+
+unset($_, $a);
